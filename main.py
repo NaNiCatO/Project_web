e49ef05a7027646b37dd13814b24193ee9bd634d
Blue2055 <124979783+Blue2055@users.noreply.github.com>
2023-10-13 23:40:24 +0700

    login 50% done

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request, Form, HTTPException
-from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.responses import HTMLResponse, FileResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 import uvicorn
 import os
@@ -11,6 +11,28 @@ app.mount("/css", StaticFiles(directory="css"), name="static_css")
 app.mount("/js", StaticFiles(directory="js"), name="static_js")
 
 
+#*********************************************************
+import ZODB , ZODB.FileStorage
+import transaction
+import BTrees.OOBTree
+
+mystorage = ZODB.FileStorage.FileStorage('mydata.fs')
+db = ZODB.DB(mystorage)
+connection = db.open()
+root = connection.root
+
+class User():
+    def __init__(self , username , password , email):
+        self.username = username
+        self.password = password
+        self.email = email
+
+#Create admin user
+root.user_data = BTrees.OOBTree.BTree()
+root.user_data['admin'] = User('admin1' , 'admin2' , 'admin@gmail.com')
+transaction.commit()
+
+connection.close()
 
 #______________________home______________________
 @app.get("/", response_class=HTMLResponse)
@@ -44,9 +66,40 @@ if not os.path.exists(data_dir):
 async def get_register_page(request: Request):
     return FileResponse("page/register.html")
 
+@app.get("/register/{username}/{password}/{email}")
+async def register(username: str , password: str , email: str):
+    user_data = root.user_data
+    for user in user_data:
+        user_info = user_data[user]
+        if username == user_info.username:
+            return  JSONResponse(content={"message": "Username is already taken"})
+        if email == user_info.email:    
+            return JSONResponse(content={"message": "Email is already taken"})
+        
+    # put info in database
+    connection = db.open()
+    root.user_data[username] = User(username , password , email)
+    transaction.commit()
+    connection.close()
+    return JSONResponse(content={"message": "Register successfully"})
+
+@app.get("/register/all_data")
+async def get_all_data():
+    user_data = root.user_data
+    data = []
+    for user in user_data:
+        user_info = user_data[user]
+        data.append({
+            "username": user_info.username,
+            "password": user_info.password,
+            "email": user_info.email
+        })
+    return JSONResponse(content={"data": data})
+
 
 @app.post("/process_register")
 async def process_register(email: str = Form(...), username: str = Form(...), password: str = Form(...)):
+
     user_data = {
         "email": email,
         "username": username,
@@ -62,10 +115,14 @@ async def process_register(email: str = Form(...), username: str = Form(...), pa
     return FileResponse("page/login.html")
     
 
-#______________________meeting______________________
+
 @app.get("/meeting", response_class=HTMLResponse)
 async def get_meeting_page(request: Request):
     return FileResponse("page/meeting.html")
 
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+connection.close()
